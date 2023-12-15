ce753a1abcd005bfb07b6c0ef69731367246dd7b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2023-12-15 04:41:16 +0100

    Initial progress

---
+++ index.js
@@ -0,0 +1,46 @@
+//Log message upon starting the bot
+// eslint-disable-next-line no-console
+console.log('Bot is starting...');
+
+require('dotenv').config();
+
+//require the discord.js module
+const Discord = require('discord.js');
+//create a Discord client with discord.js
+const client = new Discord.Client({
+	intents: [
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.GuildMessageReactions,
+		Discord.GatewayIntentBits.DirectMessages,
+		Discord.GatewayIntentBits.DirectMessageReactions,
+	],
+	partials: [
+		Discord.Partials.Message,
+		Discord.Partials.Reaction,
+		Discord.Partials.Channel,
+	]
+});
+
+//login with the Discord client using the Token from the .env file
+// eslint-disable-next-line no-undef
+client.login(process.env.BOTTOKEN);
+
+//declare what the discord client should do when it's ready
+client.on('ready', readyDiscord);
+
+//declare the function which will be used when ready
+function readyDiscord() {
+	//log a message when ready
+	// eslint-disable-next-line no-console
+	console.log('The Bot is ready.');
+
+	client.user.setPresence({
+		status: 'online',  //You can show online, idle....
+		activities: [{
+			name: 'with Elitebotix',  //The message shown
+			// type: 'PLAYING' //PLAYING: WATCHING: LISTENING: STREAMING:
+		}]
+	});
+
+	// Keeping track of reminders
+}
