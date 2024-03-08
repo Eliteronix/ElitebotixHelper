4e86a7f254161e3bc0c3801ca7b94ec3c3e609e5
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-03-08 19:02:06 +0100

    Added automatic message publishing

--- messageCreate.js
+++ messageCreate.js
@@ -3,4 +3,16 @@ module.exports = async function (msg) {
 		// eslint-disable-next-line no-console
 		console.log(msg.client.reminderUsers);
 	}
+
+	if (msg.guildId === '727407178499096597') {
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+
+		if (msg.channel.type === 5) { // Announcements
+			try {
+				await msg.crosspost();
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
 };
