de119f7adc84e4220979dcf4ee54b6bc75974a97
Eliteronix <zimmermann.mariomarvin@gmail.com>
2024-06-30 16:58:39 +0200

    Added poll content

--- commands/premier-poll.js
+++ commands/premier-poll.js
@@ -24,5 +24,28 @@ module.exports = {
 				)),
 	async execute(interaction) {
 		await interaction.deferReply();
+
+		const map = interaction.options.getString('map');
+
+		const poll = {
+			question: `When can you play? | ${map}`,
+			answers: [
+				'(Scrim) Wednesday 7PM',
+				'(Match) Thursday 7PM',
+				'(Scrim) Friday 8PM',
+				'(Match) Saturday 8PM',
+				'(Match) Sunday 7PM',
+			],
+			duration: 192, // 8 days
+			allow_multiselect: true,
+			layout_type: 1,
+		};
+
+		let response = await interaction.editReply({ poll: poll });
+
+		// Pin the poll
+		await interaction.channel.messages.fetch(response.id)
+			.then(message => message.pin())
+			.catch(console.error);
 	},
 };
